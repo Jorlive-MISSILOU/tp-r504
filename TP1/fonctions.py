7c954a13422b6e09dd93bba22d2986e56bee53fb
Jorlive.MISSILOU <Jorlive.MISSILOU>
2023-09-08 09:09:23 +0200

    ajout du nouveau fichier fonctions

---
+++ TP1/fonctions.py
@@ -0,0 +1,9 @@
+def puissance():
+    
+    a= input("Veuillez entrer la valeur de a: ")
+    a=int(a)
+    b= input("La valeur de b: ")
+    b=int(b)
+    print("a exposant b = ", a**b)
+    
+puissance()
--- TP1/prog1.py
+++ TP1/prog1.py
@@ -1,5 +1,6 @@
 print("Hello, world !")
 
-entree= input("Veuillez entrer un nombre: ")
-entree=int(entree)
-print("Son carré est: ", entree**2)
+while True:
+    entree= input("Veuillez entrer un nombre: ")
+    entree=int(entree)
+    print("Son carré est: ", entree**2)
