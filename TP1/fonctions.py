b485dbc3e45de866a841d1f9b8f4b6d665d4d637
Jorlive.MISSILOU <Jorlive.MISSILOU>
2023-09-09 09:50:28 +0200

    ajout des cas limites

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -4,9 +4,12 @@ def puissance(a,b):
 
     if not (type(a) is int and type(b) is int):
         raise TypeError("Only integers are allowed")
+    if a == 0 and b == 0:
+        raise ValueError("0 exposant 0 n'est pas définie.")
+    if b < 0:
+        raise ValueError("L'exposant ne doit pas etre negatif.")
     
-    expo= a**b
-    return expo
+    return a ** b
     
 #puissance(2,6)
     
--- TP1/test.py
+++ TP1/test.py
@@ -10,16 +10,16 @@ def test_2() :
     assert f.puissance(-2,2) == 4
 
 def test_3():
-    if (f.b <=0):
-        raise TypeError("Pas défini")
-    
-    assert ( f.puissance (2 ,2) == 4 )
-    assert ( f.puissance ( -2 ,2) == 4 )
-    assert ( f.puissance (2 ,0) == 1 )
-    assert ( f.puissance ( -2 ,0) == -1 )
-    assert ( f.puissance (0 ,3) == 0 )
+    assert f.puissance (2 ,2) == 4 
+    assert f.puissance ( -2 ,2) == 4
+    assert f.puissance (2 ,0) == 1
+    assert f.puissance ( -2 ,0) == 1
+    assert f.puissance (0 ,3) == 0
+    #with pytest.raises(ValueError):
+        #f.puissance (0 ,0)
+        #f.puissance (0 , -3)
     with pytest.raises(ValueError):
-        f.puissance (0 ,0)
-        f.puissance (0 , -3)
-        
-        
+        f.puissance(0, 0)
+
+    with pytest.raises(ValueError):
+        f.puissance(0, -3)
